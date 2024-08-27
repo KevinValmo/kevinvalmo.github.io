b75309ddcb4bbbb521c5fb8c6354b5e4e4bfbc74
Kevin Valmorbida <kevinvalmo@gmail.com>
2024-08-27 16:35:39 +0200

    .

--- src/app/pages/blog/[slug].page.ts
+++ src/app/pages/blog/[slug].page.ts
@@ -21,7 +21,7 @@ import TagBadgeComponent from "../../core/components/tag-badge.component";
       <div class="flex flex-col gap-2 ">
         <div class="text-4xl font-bold">{{ post.attributes.title }}</div>
         <div class="italic">{{ post.attributes.description }}</div>
-        <div class="flex gap-2 ml-auto mr-0">
+        <div class="flex gap-2 ml-auto mr-0 flex-wrap">
           @for (tag of post.attributes.tags.split(','); track $index) {
             <kvsrc-tag-badge [tag]="tag" />
           }
