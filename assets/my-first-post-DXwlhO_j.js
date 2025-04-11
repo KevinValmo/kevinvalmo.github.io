4e545df1003332d4c18a91e787d2015532a98f1e
KevinValmo <KevinValmo@users.noreply.github.com>
2025-04-11 14:25:37 +0000

    deployed via Analog Publish Github Pages 🚀 for 77ee39db5fdca7e01169ef6d7cbc48d8cb26d1bb

---
+++ assets/my-first-post-DXwlhO_j.js
@@ -0,0 +1,24 @@
+const e=`---
+title: Welcome to my blog
+slug: my-first-post
+date: 2024-08-26
+description: My first blog post on my new blog, powered by Analog, the Angular meta-framework.
+coverImage: ponale-garda-lake.jpg
+coverImageLow: ponale-garda-lake.low.jpg
+coverImageTitle: Ponale, Garda Lake - Kevin Valmorbida
+tags: c#, typescript, angular, ui-library, tailwindcss
+---
+
+<h2 id="long-time-no-see">Long Time No See</h2>
+<p>It&#39;s been a while since my last blog update. Life has kept me busy with work and various projects, but I&#39;m thrilled to be back and eager to share my journey with you. I&#39;ve been working extensively with Angular, and while I&#39;m still honing my skills, I&#39;ve gained valuable experience through home projects and experimentation.</p>
+<h2 id="building-a-custom-ui-component-library">Building a Custom UI Component Library</h2>
+<p>One of the most rewarding projects I&#39;ve tackled recently is the creation of a custom UI component library for Angular, inspired by the <a href="https://daisyui.com/">daisyUI</a> library for <a href="https://tailwindcss.com/">TailwindCSS</a>. While building such a library can be challenging, it&#39;s also incredibly fulfilling. Through this project, I deepened my understanding of templating, styling, and the Angular component lifecycle. With these new skills, I feel more confident than ever in my ability to take on any project. Though there&#39;s always more to learn, I&#39;m excited to continue growing and improving.</p>
+<h2 id="should-i-use-my-custom-library-in-my-next-project">Should I Use My Custom Library in My Next Project?</h2>
+<p>The short answer? Probably not. While custom libraries can be powerful, there are plenty of excellent UI libraries out there that are well-maintained, thoroughly documented, and open-source, like <a href="https://primeng.org/">primeng</a>. For small to medium-sized teams, particularly those in product development, it&#39;s usually more efficient to leverage existing solutions rather than reinventing the wheel. This allows you to focus on what truly matters: the business logic and user experience of your application.</p>
+<h2 id="enter-analog-the-angular-meta-framework">Enter Analog: The Angular Meta-Framework</h2>
+<p>A few months ago, I stumbled upon <a href="https://analogjs.org/">Analog</a> while searching for an Angular blog template. I had previously attempted to create my own blog template but had to put it on hold due to time constraints. Thankfully, I&#39;ve recently found the time to revisit this project, and setting up a blog with the Analog template was a breeze. Analog, of course, is not a blog template, it&#39;s a meta-framework that allows you to build Angular applications quickly and efficiently by providing a set of pre-built components, services, and utilities like <a href="https://angular.io/guide/ssr">SSR</a>, <a href="https://angular.dev/guide/prerendering">SSG</a>, <a href="https://analogjs.org/docs/features/routing/overview">file based routing</a>, and more. Like what Next.js is to React, Analog is to Angular.</p>
+<h2 id="this-blogs-setup">This Blog&#39;s Setup</h2>
+<p>The blog isn&#39;t completed yet but here please find the link to this blog&#39;s <a href="https://github.com/KevinValmo/kevinvalmo.github.io">GitHub Repo</a>. I appreciate any feedback you may have on the blog&#39;s design, content, or functionality and don&#39;t forget to star the repo if you find it useful.</p>
+<h2 id="conclusion-1">Conclusion</h2>
+<p>I&#39;m thrilled to be back and excited to share my thoughts and experiences with you. I hope you enjoy the content on my blog and find it useful. I&#39;ll do my best to keep it updated regularly. If you have any questions or suggestions, please feel free to leave a comment, I&#39;m always happy to help. Thank you for reading, and I look forward to connecting with you soon!</p>
+`;export{e as default};
